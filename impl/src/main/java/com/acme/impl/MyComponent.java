d72781734dcc3ffe62b47927e98ae222fb528b94
Romain Grecourt <romain.grecourt@oracle.com>
2023-02-21 19:19:56 -0800

    cleanup

--- impl/src/main/java/com/acme/impl/MyComponent.java
+++ impl/src/main/java/com/acme/impl/MyComponent.java
@@ -1,11 +1,9 @@
 package com.acme.impl;
 
-import com.acme.api.Component;
 import com.acme.api.VNode;
 
 import static com.acme.api.VNodeProcessor.HTML;
 
-@Component
 public class MyComponent {
 
     private String name = "John";
